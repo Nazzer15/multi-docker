00ba47c78a9d1e009fa4bef6dcceec57b9becb1c
Jose David Jimenez <jdjimenez@ibm.com>
2021-08-05 09:18:13 -0600

    Initial commit

---
+++ client/src/Otherpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const otherpage = () => {
+  return (
+    <div>
+      I'm some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default otherpage;
+
+
